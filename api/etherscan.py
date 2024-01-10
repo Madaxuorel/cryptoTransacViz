0872dee6fdec452e6775046a87b4ff300ebc5149
Adam Le Roux <adamleroux26@gmail.com>
2024-01-10 15:11:53 +0100

    add api comments

--- api/etherscan.py
+++ api/etherscan.py
@@ -3,21 +3,19 @@ import json
 
 ENDPOINT = "https://api.etherscan.io/api"
 
-"""
-when given an eth address, we will query :
-1 : ACCOUNT BALANCE
-2 : TOP ACCOUNTS BY TRANSACTION VOLUME
-3 : ENS ? 
-4 : TOP TRANSACTIONS BY SIZE
-5 : MISC STATS ? (TOTAL GAS, TOTAL TRANS VOL)
-"""
 
-
+"""
+Object that interfaces with the etherscan api
+Limited to 5 calls per sec in free plan
+"""
 class etherScanApi:
 
     def __init__(self, key):
         self.key = key
         
+    """
+    Returns net eth balance of a given account.
+    """
     def getETHBalance(self,account):
         
         url = f"https://api.etherscan.io/api?module=account&action=balance&address={account}&tag=latest&apikey={self.key}"
@@ -25,22 +23,34 @@ class etherScanApi:
         amount = response.json().get('result')
         return float(amount/1000000000000000000)
     
+    """
+    Returns eth/usd pair value at current time.
+    """
     def getEthValue(self):
         
         url = f"https://api.etherscan.io/api?module=stats&action=ethprice&apikey={self.key}"
         ethUsd = requests.get(url).json().get('result').get('ethusd')
         return float(ethUsd)
     
+    """
+    Returns balance in a given account in usd, at current pair value.
+    """
     def getUSDBalance(self,account):
         
         return self.getETHBalance(account) * float(self.getEthValue)
     
+    """
+    Returns list of latest transactions by a given account (to and from). Max 10k transactions.
+    """
     def getLatestTransactions(self,account):
         
         url = f"https://api.etherscan.io/api?module=account&action=txlist&address={account}&startblock=0&endblock=9999999999999&page=1&offset=10000&sort=asc&apikey={self.key}"
         response = requests.get(url).json().get('result')
         return response
     
+    """
+    Returns the sorted top transactions by number of transactions received from same account.
+    """
     def getTopAddressesReceived(self,account):
         
         self.transactions = self.getLatestTransactions(account)
@@ -56,12 +66,17 @@ class etherScanApi:
         
         return addressesReceived
     
-    
+    """
+    Returns top N addresses by received transaction count. 
+    """
     def getTopNAddressesReceived(self,account,N):
         
         addressesReceived = self.getTopAddressesReceived(account)
         return {k: v for i, (k, v) in enumerate(addressesReceived.items()) if i < N}
-        
+    
+    """
+    Returns the sorted top transactions by number of transactions sent to same account.
+    """
     def getTopAddressesSent(self,account):
         self.transactions = self.getLatestTransactions(account)
         addressesSent = {}
@@ -75,11 +90,17 @@ class etherScanApi:
 
         return addressesSent
     
+    """
+    Returns top N addresses by sent transaction count. 
+    """
     def getTopNAddressesSent(self,account,N):
         
         addressesSent = self.getTopAddressesSent(account)
         return {k: v for i, (k, v) in enumerate(addressesSent.items()) if i < N}
     
+    """
+    Return total value of transaction from/to the given account.
+    """
     def getEthValueTransferred(self,account,transactionType):
         total = 0
         print(account)
